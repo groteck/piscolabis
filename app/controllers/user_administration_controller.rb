723a23f25a29afbd7a9dfef3e44e93ff3aee6a77
DavidKang <gerheman@gmail.com>
2011-12-03 18:00:11 +0000

    modificado user_administration

--- app/controllers/user_administration_controller.rb
+++ app/controllers/user_administration_controller.rb
@@ -18,15 +18,39 @@ class UserAdministrationController < ApplicationController
 
     def create
         @user = User.new(params[:id])
+
+        respond_to do |format|
+            if @user.save
+                format.html { redirec_to @user }
+                format.json { render json: @user, status: :created, location: @user }
+            else
+                format.html { render action: "new" }
+                format.json {render json: @user.errors, status: :unprocessable_entity }
+            end
+        end
     end
 
     def update
         @user = User.find(params[:id])
+
+        respond_to do |format|
+            if @user.update_attributes(params[:user])
+                format.html { redirect_to @user }
+                format.json { head :ok }
+            else
+                format.html { render action: "edit" }
+                format.json { render json: @user.errors, status: :unprocessable_entity }
+            end
+        end
     end
 
     def destroy
         @user = User.find(params[:id])
         @user.destroy
-    end
 
+        respond_to do |format|
+            format.html { redirect_to types_url }
+            format.json { head :ok }
+        end
+    end
 end
